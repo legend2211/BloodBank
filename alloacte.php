f8fc417d528ad53eff51fec7a5fc3c701807c7d3
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-24 20:50:52 +0530

    Update alloacte.php

--- alloacte.php
+++ alloacte.php
@@ -1,40 +1,22 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Blood Bank System</title>
+    <link rel="stylesheet" type="text/css" href="style2.css"/>
+</head>
 <body bgcolor="white">
-<title>Blood Bank System</title>
-<link rel="stylesheet" type="text/css" href="style2.css"/>
 <div class="topnav">
- 
-<a class="active " href="adminhome.php">Home</a>
-                <a class="active " href="choice.html">Register</a>
-                <a class="active " href="search.php">Search</a>
-                <a class="active " href="adminrequest.php">Requests</a>
-                
-                <a class="active" href="alloacte.php">alloacte</a>
-                <a class="active" href="report.php">Report</a>
- 
+    <a class="active" href="adminhome.php">Home</a>
+    <a class="active" href="choice.html">Register</a>
+    <a class="active" href="search.php">Search</a>
+    <a class="active" href="adminrequest.php">Requests</a>
+    <a class="active" href="alloacte.php">Allocate</a>
+    <a class="active" href="report.php">Report</a>
 </div>
 <center>
-<body>
     <div class="container">
         <form action="" method="POST">
-
-            <h1> Serch For Blood </h1>
-
-
-           <!-- <label>First Name</label>
-            <input type="text" name="fname" pattern="[a-zA-Z]+" title="Invalid name Format" required> <br><br>
-
-            <label>Last Name</label>
-            <input type="text" name="lname" pattern="[a-zA-Z]+" title="Invalid name Format" required><br><br>
-
-            <label>Contact No</label>
-            <input type="Text" name="cno" pattern="[0-9]{10}" title="Contact no must be of 10 digits" required><br><br>
-
-            <label>Email id </label>
-            <input type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                title="Invalid Email Format" required> <br><br>-->
-
+            <h1>Search For Blood</h1>
             <label>Select Blood Group:</label>
             <select name="bgroup" required>
                 <option>O+</option>
@@ -45,107 +27,78 @@
                 <option>A-</option>
                 <option>B-</option>
                 <option>AB-</option>
-            </select> <br> <br>
-
-            
-
-
-
-            <input type="submit" value="serch"> &nbsp;
-            <button><a href="home.php"> Back To Home</a></button>
-
-
-
-
+            </select><br><br>
+            <input type="submit" value="Search">&nbsp;
+            <button><a href="home.php">Back To Home</a></button>
+        </form>
     </div>
-    </form>
-</body>
 
-</html>
+    <?php
+    if (isset($_POST['bgroup'])) {
+        $bgroup = $_POST['bgroup'];
 
-<?php
+        try {
+            $cn = mysqli_init();
+            mysqli_ssl_set($cn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+            mysqli_real_connect($cn, "myazsqldemo.mysql.database.azure.com", "myadmin@myazsqldemo", "Server@1", "project", 3306);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
 
-if(isset($_POST['bgroup']))
-{
-  $bgroup=$_POST['bgroup'];
-  
-  try
-{
-    $host="localhost";
-    $port=3306;
-    $dbname="project";
-    $dbuser="root";
-    $dbpass="root";
+        $sql = "SELECT * FROM register WHERE lasttime <= NOW() - INTERVAL 3 MONTH AND bloodgroup='$bgroup'";
+        $result = $cn->query($sql);
 
-    $cn=new PDO("mysql:host=$host; port=$port; dbname=$dbname",$dbuser,$dbpass);
-}
+        if ($result) {
+            $rowcount = mysqli_num_rows($result);
+        } else {
+            $rowcount = 0;
+        }
+        ?>
 
-catch(PDOException $e)
-{
-    echo $e-> getmessage();
-    die();
-}
+        <html>
+        <head>
+            <style>
+                td, th {
+                    padding: 10px;
+                }
 
-$sql="SELECT  * FROM register WHERE lasttime <= NOW() - INTERVAL 3 MONTH and bloodgroup='$bgroup'";
-$result=$cn->query($sql);
-
-$rowcount=$result->rowCount();
-?>
-<html>
-    <head>
-        <style>
-            td,th{
-                padding:10px;
+                table {
+                    margin-top: 20px;
+                    width: 350px;
+                }
+            </style>
+        </head>
+        <body>
+        <h2>Search Results</h2>
+        <?php
+        if ($rowcount > 0) {
+            echo "<table border='1' align='center' style='border-collapse:collapse;'>
+                    <tr>
+                        <th>Donor Name</th>
+                        <th>Blood Group</th>
+                        <th>Last Time</th>
+                        <th>Contact No</th>
+                    </tr>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>
+                        <td>" . $row['firstname'] . " " . $row['lastname'] . "</td>
+                        <td>" . $row['bloodgroup'] . "</td>
+                        <td>" . $row['lasttime'] . "</td>
+                        <td>" . $row['contactno'] . "</td>
+                      </tr>";
             }
-            table{
-                margin-top:20px;
-                width:350px;
-            }
-        </style>
-    </head>
-    <body>
-<?php
-
-echo"<table border=1 align='center' style='border-collapse:collapse';>
-        <tr>
-            
-            <th>donor name</th>
-            <th>Blood Group</th>
-            <th>Last Time</th>
-            <th>contact no</th>
-        </tr>";
-if($rowcount>0)
-{
-    foreach($result as $row)
-    {
-        echo"<tr>
-                
-                <td>".$row[0]." ".$row[1]."</td>
-                <td>".$row[3]."</td>
-                <td>".$row[7]."</td>
-                <td>".$row[8]."</td>
-        </tr>";
+            echo "</table>";
+        } else {
+            echo "$bgroup is not Available";
+        }
+        ?>
+        </body>
+        </html>
+        <?php
+        mysqli_close($cn);
     }
-    echo "</table>";
-}
-else 
-{
-    echo"$bgroup is not Available";
-/*?>
-    <script type="text/javascript">alert('No data found !!!');</script>
-    <?php*/
-}
-?>
+    ?>
+</center>
 </body>
 </html>
-<?php
-
-
-}
-
-
-?>
-    
-
-
-
