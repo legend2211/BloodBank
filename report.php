b779c6a823d7c91a6b0e43786c1d6c2dbf1e5c16
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-24 20:43:43 +0530

    Update report.php

--- report.php
+++ report.php
@@ -1,110 +1,62 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
-
 <head>
-    <center>
-
+    <title>Blood Bank System</title>
+    <link rel="stylesheet" type="text/css" href="style2.css" />
+</head>
 <body bgcolor="whitesmoke">
-    <form name="Home" method="post">
-        <table>
-            <title>Blood Bank System</title>
-            <link rel="stylesheet" type="text/css" href="style2.css" />
-            <div class="topnav">
-            <a class="active " href="adminhome.php">Home</a>
-                <a class="active " href="choice.html">Register</a>
-                <a class="active " href="search.php">Search</a>
-                <a class="active " href="adminrequest.php">Requests</a>
-                
-                <a class="active" href="alloacte.php">alloacte</a>
-                <a class="active" href="report.php">Report</a>
-             
-            </div>
-            <body>
+    <div class="topnav">
+        <a class="active" href="adminhome.php">Home</a>
+        <a class="active" href="choice.html">Register</a>
+        <a class="active" href="search.php">Search</a>
+        <a class="active" href="adminrequest.php">Requests</a>
+        <a class="active" href="alloacte.php">Allocate</a>
+        <a class="active" href="report.php">Report</a>
+    </div>
 
-<?php
-  
-  /*if(isset($_POST['bgroup']))
-  {
-  $bgroup=$_POST['bgroup'];*/
+    <?php
+    try {
+        $cn = mysqli_init();
+        mysqli_ssl_set($cn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+        mysqli_real_connect($cn, "myazsqldemo.mysql.database.azure.com", "myadmin@myazsqldemo", "Server@1", "project", 3306);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        die();
+    }
 
-  try
-{
-    /* $host="localhost";
-    $port=3306;
-    $dbname="project";
-    $dbuser="root";
-    $dbpass="root";
+    $sql = "SELECT id, firstname, lastname, contactno, bloodgroup FROM register";
+    $result = $cn->query($sql);
 
-    $cn=new PDO("mysql:host=$host; port=$port; dbname=$dbname",$dbuser,$dbpass);*/
-    $cn=mysqli_init(); mysqli_ssl_set($cnn, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-    mysqli_real_connect($con, "myazsqldemo.mysql.database.azure.com", "myadmin@myazsqldemo", {Server@1}, {project}, 3306);
+    if ($result) {
+        $rowcount = mysqli_num_rows($result);
+    } else {
+        $rowcount = 0;
+    }
+    ?>
 
-}
-
-catch(PDOException $e)
-{
-    echo $e-> getmessage();
-    die();
-}
-
-$sql="select id,firstname,lastname,contactno,bloodgroup from register ";
-$result=$cn->query($sql);
-
-$rowcount=$result->rowCount();
-?>
-<html>
-    <head>
-        <style>
-            td,th{
-                padding:10px;
-            }
-            table{
-                margin-top:20px;
-                width:350px;
-            }
-        </style>
-    </head>
-    <body>
-        <h1> Report <h1>
-<?php
-
-echo"<table border=1 align='center' style='border-collapse:collapse';>
+    <h1>Report</h1>
+    <table border="1" align="center" style="border-collapse:collapse;">
         <tr>
             <th>Id</th>
             <th>Donor Name</th>
             <th>Contact No</th>
             <th>Blood group</th>
-        </tr>";
-if($rowcount>0)
-{
-    foreach($result as $row)
-    {
-        echo"<tr>
-                <td>".$row[0]."</td>
-                <td>".$row[1]." ".$row[2]."</td>
-                <td>".$row[3]."</td>
-                <td>".$row[4]."</td>
-        </tr>";
-    }
-    echo "</table>";
-}
-else 
-{
-    echo"$bgroup is not Available";
-?>
-    <script type="text/javascript">alert('No data found !!!');</script>
-<?php
-}
-?>
+        </tr>
+        <?php
+        if ($rowcount > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['firstname'] . " " . $row['lastname'] . "</td>";
+                echo "<td>" . $row['contactno'] . "</td>";
+                echo "<td>" . $row['bloodgroup'] . "</td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='4'>No data found</td></tr>";
+        }
+        ?>
+    </table>
+    <?php mysqli_close($cn); ?>
 </body>
-</html>
-<?php
-
-?>
- </body>
-      
-    
-   
-    </head>
-
 </html>
